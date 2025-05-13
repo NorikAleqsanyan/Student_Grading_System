5f037e0719551985e366a1f1901fb476f4b61f9e
NorikAleqsanyan <aleqsanyan.004@gmail.com>
2025-05-13 12:51:30 +0300

    docs: add JSDoc comments to homework service

--- src/homework/homework.service.ts
+++ src/homework/homework.service.ts
@@ -6,6 +6,9 @@ import { Repository } from 'typeorm';
 import { CreateHomeworkDto } from './dto/create-homework.dto';
 import { Homework } from './entities/homework.entity';
 
+/**
+ * Service for managing homework assignments.
+ */
 @Injectable()
 export class HomeworkService {
   constructor(
@@ -15,31 +18,60 @@ export class HomeworkService {
     @InjectRepository(Model) private modelRepository: Repository<Model>,
   ) {}
 
+  /**
+   * Creates a new homework assignment.
+   * 
+   * @param {CreateHomeworkDto} createHomeworkDto - The data to create a new homework assignment.
+   * @returns {Promise<Homework | { message: string, error: boolean }>} 
+   * - Returns the created homework object if successful.
+   * - Returns an error message if the group or model is not found or if the model is not active in the group.
+   */
   async create(createHomeworkDto: CreateHomeworkDto) {
-    const { modelId, groupId } = createHomeworkDto;
+    const { modelId, groupId, title, body } = createHomeworkDto;
+    
     const group = await this.groupRepository.findOneBy({ id: groupId });
     if (!group) {
+
       return { message: 'Group not found', error: true };
     }
+
     const model = await this.modelRepository.findOneBy({ id: modelId });
     if (!model) {
+
       return { message: 'Model not found', error: true };
     }
-    if (group.activeModelId != model.id) {
+
+    if (group.activeModelId !== model.id) {
+
       return { message: 'This model is not active in group', error: true };
     }
+
     return this.homeworkRepository.save({
-      ...createHomeworkDto,
+      modelId,
+      groupId, 
+      title, 
+      body,
       date: new Date(),
     });
   }
 
+  /**
+   * Removes a homework assignment by ID.
+   * 
+   * @param {number} id - The ID of the homework to remove.
+   * @returns {Promise<{ message: string, error: boolean }>} 
+   * - Returns a success message if the homework is successfully deleted.
+   * - Returns an error message if the homework is not found.
+   */
   async remove(id: number) {
     const homework = await this.homeworkRepository.findOne({ where: { id } });
     if (!homework) {
+
       return { message: 'Homework not found', error: true };
     }
+
     await this.homeworkRepository.remove(homework);
-    return { message: 'Homework delete', error: false };
+    
+    return { message: 'Homework deleted', error: false };
   }
 }
